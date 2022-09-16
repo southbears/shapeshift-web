3b365f85f2724fea76ea974ca1e9cd261d195e63
southbear <108498243+southbears@users.noreply.github.com>
2022-09-17 00:15:07 +0200

    feat: advanced parameters form fields

---
+++ src/plugins/walletConnectToDapps/components/modal/ModalSection.tsx
@@ -0,0 +1,36 @@
+import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons'
+import { Box, Button, Collapse } from '@chakra-ui/react'
+import type { FC, ReactElement, ReactNode } from 'react'
+import { useCallback, useState } from 'react'
+import { RawText } from 'components/Text'
+
+type Props = {
+  title: string
+  icon?: ReactElement
+  defaultOpen?: boolean
+  children: ReactNode
+}
+
+export const ModalSection: FC<Props> = ({ title, icon, children, defaultOpen = true }) => {
+  const [isOpen, setOpen] = useState(defaultOpen)
+  const toggle = useCallback(() => setOpen(prev => !prev), [])
+  return (
+    <Box>
+      <Button
+        width='full'
+        variant='link'
+        textAlign='left'
+        justifyContent='flex-start'
+        leftIcon={<Box color='gray.500'>{icon}</Box>}
+        rightIcon={
+          isOpen ? <ChevronDownIcon color='gray.500' /> : <ChevronUpIcon color='gray.500' />
+        }
+        mb={4}
+        fontWeight='medium'
+        children={<RawText flex={1}>{title}</RawText>}
+        onClick={toggle}
+      />
+      <Collapse in={isOpen}>{children}</Collapse>
+    </Box>
+  )
+}
